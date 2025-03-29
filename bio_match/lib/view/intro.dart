fbb2413682d2a2f9dead02a2762017aa08d285fb
matifere <matifere@gmail.com>
2025-03-29 05:28:05 -0300

    pagina intro y post en pagina principal

--- bio_match/lib/view/intro.dart
+++ bio_match/lib/view/intro.dart
@@ -1,3 +1,4 @@
+import 'package:bio_match/view/pagina_principal.dart';
 import 'package:flutter/material.dart';
 
 class Intro extends StatefulWidget {
@@ -8,47 +9,167 @@ class Intro extends StatefulWidget {
 }
 
 class _IntroState extends State<Intro> {
+  int currentPage = 0;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Stack(
         children: [
           PageView(
+            onPageChanged: (value) {
+              setState(() {
+                currentPage = value;
+              });
+            },
             children: [
-              Container(
-                color: Colors.red,
-                child: const Center(
-                  child: Text('Pantalla 1',
-                      style: TextStyle(
-                          fontSize: 40,
-                          fontFamily: 'DoppioOne',
-                          color: Color.fromARGB(255, 0, 0, 0))),
+              Explanation(
+                titulo: Text(
+                  "¡Hola!\nSoy BioMatch",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontSize: 32,
+                    fontFamily: 'DoppioOne',
+                    color: Color.fromARGB(255, 0, 0, 0),
+                  ),
+                ),
+                imagen: Image.asset(
+                  'assets/images/hands.png',
+                  width: 229,
+                  height: 166,
+                ),
+                texto: Text(
+                  "Tu aplicación para darle valor a los residuos orgánicos que hoy estan contaminando nuestro planeta",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontSize: 20,
+                    fontFamily: 'DoppioOne',
+                    color: Colors.black,
+                  ),
                 ),
               ),
-              Container(
-                color: Colors.green,
-                child: const Center(
-                  child: Text('Pantalla 2',
-                      style: TextStyle(
-                          fontSize: 40,
-                          fontFamily: 'DoppioOne',
-                          color: Color.fromARGB(255, 0, 0, 0))),
+              Explanation(
+                titulo: Text(
+                  "Conmigo podes ofrecer y retirar residuos orgánicos de forma simple y ágil, dándoles un segundo uso y reduciendo su impacto ambiental",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontSize: 20,
+                    fontFamily: 'DoppioOne',
+                    color: Colors.black,
+                  ),
                 ),
-              ),
-              Container(
-                color: Colors.blue,
-                child: const Center(
-                  child: Text('Pantalla 3',
+                imagen: Image.asset('assets/images/trash.png'),
+                texto: Column(
+                  spacing: 16,
+                  children: [
+                    Text(
+                      "En pocas palabras...",
                       style: TextStyle(
-                          fontSize: 40,
-                          fontFamily: 'DoppioOne',
-                          color: Color.fromARGB(255, 0, 0, 0))),
+                        fontSize: 18,
+                        fontFamily: 'DoppioOne',
+                        color: Colors.black,
+                      ),
+                    ),
+
+                    Text("¡Te beneficias vos y beneficias al planeta!",
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        fontSize: 20,
+                        fontFamily: 'DoppioOne',
+                        color: Colors.black,
+                      ),
+                    ),
+                    FilledButton(onPressed: (){
+                      Navigator.of(context).push(MaterialPageRoute(builder: (context) => PaginaPrincipal()));
+                    },
+                    style: ButtonStyle(
+                      backgroundColor: WidgetStateProperty.all<Color>(
+                        Color.fromARGB(255, 0, 0, 0),
+                      ),
+                      shape: WidgetStateProperty.all<RoundedRectangleBorder>(
+                        RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(18.0),
+                        ),
+                      ),
+                    ), child: Text("¡Comencemos!",
+                      style: TextStyle(
+                        fontSize: 16,
+                        fontFamily: 'DoppioOne',
+                        color: Colors.white,
+                      ),
+                    ),
+                    )
+
+                  ],
                 ),
               ),
             ],
+          ),
+
+          Align(
+            alignment: Alignment.bottomCenter,
+            child: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: List.generate(
+                  2,
+                  (index) => Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 4.0),
+                    child: Dot(isActive: index == currentPage,),
+                  ),
+                ),
+              ),
+            ),
           )
         ],
-      )
+      ),
+    );
+  }
+}
+
+class Dot extends StatelessWidget {
+  const Dot({
+    super.key, required this.isActive,
+  });
+  final bool isActive;
+
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedContainer(
+      width: isActive ? 20 : 10,
+      height: 10,
+      decoration: BoxDecoration(
+        color: Colors.black,
+        borderRadius: BorderRadius.circular(10),
+      ), duration: Duration(milliseconds: 500),
+      curve: Curves.easeIn,
+    );
+  }
+}
+
+class Explanation extends StatelessWidget {
+  const Explanation({
+    super.key,
+    required this.titulo,
+    required this.imagen,
+    required this.texto,
+  });
+  final Text titulo;
+  final Image imagen;
+  final Widget texto;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      spacing: 16,
+      children: [
+        Expanded(child: Container(color: Colors.transparent)),
+        titulo,
+        imagen,
+        texto,
+        Expanded(child: Container(color: Colors.transparent)),
+      ],
     );
   }
 }
---
+++ bio_match/lib/view/pagina_principal.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class PaginaPrincipal extends StatelessWidget {
+  const PaginaPrincipal({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
