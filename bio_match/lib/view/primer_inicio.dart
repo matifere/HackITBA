67ad573895f0408c22a7c329d09475193f2da52a
Thiagomg2206 <113268634+Thiagomg2206@users.noreply.github.com>
2025-03-29 02:52:42 -0300

    uwu

--- bio_match/lib/view/primer_inicio.dart
+++ bio_match/lib/view/primer_inicio.dart
@@ -1,7 +1,6 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
-
 class PrimerInicio extends StatelessWidget {
   const PrimerInicio({super.key});
 
@@ -9,7 +8,7 @@ class PrimerInicio extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Primer Inicio', style: TextStyle(),),
+        title: const Text('Primer Inicio'),
       ),
       body: Center(
         child: Column(
@@ -20,15 +19,7 @@ class PrimerInicio extends StatelessWidget {
               onPressed: () {
                 // Acción al presionar el botón
               },
-              child: StreamBuilder<DocumentSnapshot>(
-                stream: FirebaseFirestore.instance.collection('wasa').doc('wasa').snapshots(),
-                builder: (context, snapshot) {
-                  return Text(
-                    snapshot.hasData ? '¡Hola, ${snapshot.data!.get('wasa')}!' : 'Cargando...',
-                    style: const TextStyle(fontSize: 20),
-                  );	
-                }
-              ),
+              child: const Text('Continuar'), // Widget hijo requerido
             ),
           ],
         ),
