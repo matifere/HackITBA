76fcd522dd6244e4eb7eaf06b7b7e86278d58138
matifere <matifere@gmail.com>
2025-03-29 03:47:25 -0300

    commit importante

--- bio_match/lib/main.dart
+++ bio_match/lib/main.dart
@@ -1,7 +1,17 @@
+import 'package:bio_match/firebase_options.dart';
 import 'package:bio_match/view/primer_inicio.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  FirebaseFirestore.instance.settings = const Settings(
+    persistenceEnabled: true,
+  );
   runApp(const MainApp());
 }
 
--- bio_match/lib/view/primer_inicio.dart
+++ bio_match/lib/view/primer_inicio.dart
@@ -1,27 +1,32 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
-class PrimerInicio extends StatelessWidget {
+class PrimerInicio extends StatefulWidget {
   const PrimerInicio({super.key});
 
   @override
+  State<PrimerInicio> createState() => _PrimerInicioState();
+}
+
+class _PrimerInicioState extends State<PrimerInicio> {
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Primer Inicio'),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            const Text('¡Bienvenido a la aplicación!'),
-            ElevatedButton(
-              onPressed: () {
-                // Acción al presionar el botón
-              },
-              child: const Text('Continuar'), // Widget hijo requerido
-            ),
-          ],
+      
+      body: GestureDetector(
+        onTap: () {
+          Navigator.pushNamed(context, '/login');
+        },
+        child: Center(
+          child: Column(
+            children: [
+              const Text('BioMatch', 
+                  style: TextStyle(
+                      fontSize: 40,
+                      fontFamily: 'DoppioOne',
+                      color: Color.fromARGB(255, 0, 0, 0))),
+            ],
+          ),
         ),
       ),
     );
