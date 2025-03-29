18048463c167e238d61bfa7c4ba908b9a8ff9563
matifere <matifere@gmail.com>
2025-03-29 10:50:05 -0300

    inicio sesion

---
+++ bio_match/lib/view/inicio_sesion.dart
@@ -0,0 +1,100 @@
+import 'package:bio_match/classes/user.dart';
+import 'package:bio_match/view/pagina_principal.dart';
+import 'package:flutter/material.dart';
+
+// ignore: must_be_immutable
+class InicioSesion extends StatefulWidget {
+  InicioSesion({super.key});
+
+  @override
+  State<InicioSesion> createState() => _InicioSesionState();
+}
+
+class _InicioSesionState extends State<InicioSesion> {
+  late User user;
+
+  //inicializa el usuario
+  @override
+  initState() {
+    super.initState();
+    user = User(" ", " ");
+    user.setName(" ");
+    user.setDir(" ");
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          spacing: 20,
+          children: [
+            const Text(
+              'Una cosa mas...',
+              style: TextStyle(
+                fontSize: 24,
+                fontFamily: 'DoppioOne',
+                color: Color.fromARGB(255, 0, 0, 0),
+              ),
+            ),
+            const Text(
+              'Ingresa tu nombre de usuario',
+              style: TextStyle(
+                fontSize: 16,
+                fontFamily: 'DoppioOne',
+                color: Color.fromARGB(255, 0, 0, 0),
+              ),
+            ),
+            SizedBox(
+              width: 300,
+              child: TextField(
+                onChanged: (value) {
+                  user.name = value;
+                },
+                decoration: const InputDecoration(
+                  border: OutlineInputBorder(),
+                  hintText: 'Nombre de usuario',
+                ),
+              ),
+            ),
+            SizedBox(
+              width: 300,
+              child: TextField(
+                onChanged: (value) {
+                  user.dir = value;
+                },
+                decoration: const InputDecoration(
+                  border: OutlineInputBorder(),
+                  hintText: 'Dirección',
+                ),
+              ),
+            ),
+            FilledButton(
+              style: ButtonStyle(
+                backgroundColor: WidgetStateProperty.all<Color>(
+                  Color.fromARGB(255, 0, 0, 0),
+                ),
+                shape: WidgetStateProperty.all<RoundedRectangleBorder>(
+                  RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(18.0),
+                  ),
+                ),
+              ),
+              onPressed: () {
+                
+                user.sendToFirestore();
+                Navigator.of(context).push(
+                  MaterialPageRoute(
+                    builder: (context) => CategorySelectionScreen(),
+                  ),
+                );
+              },
+              child: const Text('Iniciar Sesión'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
